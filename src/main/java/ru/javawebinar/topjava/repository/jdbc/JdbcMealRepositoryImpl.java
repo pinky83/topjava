065b16886613ef7b57c9e3df79acb546c9f98a0e
pinky83 <pinky8388@gmail.com>
2016-10-02 13:05:02 +0300

    JdbcMealRepositoryImpl release (without userId restrictions)

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
@@ -1,17 +1,22 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.TimeUtil;
 
 import javax.sql.DataSource;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * User: gkislin
@@ -39,18 +44,39 @@ public class JdbcMealRepositoryImpl implements MealRepository {
     }
 
     @Override
-    public Meal save(Meal Meal, int userId) {
-        return null;
+    public Meal save(Meal meal, int userId) {
+        Objects.requireNonNull(meal);
+
+        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource()
+                .addValue("id", meal.getId())
+                .addValue("user_id", userId)
+                .addValue("date_time", meal.getDateTime())
+                .addValue("description", meal.getDescription())
+                .addValue("calories", meal.getCalories());
+
+        Integer mealId = meal.getId();
+        if (meal.isNew()) {
+            Number newKey = insertMeal.executeAndReturnKey(mapSqlParameterSource);
+            meal.setId(newKey.intValue());
+        } else if (get(mealId, userId) == null) {
+            return null;
+        }
+        namedParameterJdbcTemplate.update(
+                "UPDATE meal SET id=:id, user_id=:user_id, date_time=:date_time, " +
+                        "description=:description, calories=:calories WHERE id=:id", mapSqlParameterSource);
+        return meal;
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return jdbcTemplate.update("DELETE FROM meal WHERE id=?", id) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        List<Meal> meals = jdbcTemplate.query("SELECT * FROM meal WHERE id=?", ROW_MAPPER, id);
+        return  DataAccessUtils.singleResult(meals);
+
     }
 
     @Override
@@ -60,6 +86,10 @@ public class JdbcMealRepositoryImpl implements MealRepository {
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        Objects.requireNonNull(startDate);
+        Objects.requireNonNull(endDate);
+        return getAll(userId).stream()
+                .filter(um -> TimeUtil.isBetween(um.getDateTime(), startDate, endDate))
+                .collect(Collectors.toList());
     }
 }
