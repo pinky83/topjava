0ccc6555c291178c40d81d177df0a30d5d694967
pinky83 <pinky8388@gmail.com>
2016-10-02 14:02:36 +0300

    MealTestData is ready, MealServiceTest file created, it's empty

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -3,12 +3,27 @@ package ru.javawebinar.topjava;
 import ru.javawebinar.topjava.matcher.ModelMatcher;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 /**
  * GKislin
  * 13.03.2015.
  */
 public class MealTestData {
 
-    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>();
+    public static final Meal meal1 = new Meal(1000002, LocalDateTime.now(), "Завтрак", 350);
+    public static final Meal meal2 = new Meal(1000002, LocalDateTime.now(), "Обед", 700);
+    public static final Meal meal3 = new Meal(1000002, LocalDateTime.now(), "Ужин", 650);
+    public static final Meal meal4 = new Meal(1000002, LocalDateTime.now(), "Завтрак", 500);
+    public static final Meal meal5 = new Meal(1000002, LocalDateTime.now(), "Обед", 700);
+    public static final Meal meal6 = new Meal(1000002, LocalDateTime.now(), "Ужин", 800);
 
+    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>(
+            (expected, actual) -> expected == actual ||
+                    (   Objects.equals(expected.getId(), actual.getId()) &&
+                        Objects.equals(expected.getCalories(), actual.getCalories())&&
+                        Objects.equals(expected.getDescription(), actual.getDescription())
+                    )
+    );
 }
---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceImplTest.java
@@ -0,0 +1,41 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Дмитрий on 02.10.2016.
+ */
+public class MealServiceImplTest {
+    @Test
+    public void testGet() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testGetBetweenDateTimes() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testSave() throws Exception {
+
+    }
+
+}
